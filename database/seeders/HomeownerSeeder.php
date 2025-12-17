24be67a63138fbcf8789ce27d7b4c18cf71f4c57
ECOWWWWWWWW <jamesjerico.aquino@lorma.edu>
2025-12-18 00:43:54 +0800

    Add homeowner record to HomeownerSeeder
    Added a new homeowner record to the database seeder.

--- database/seeders/HomeownerSeeder.php
+++ database/seeders/HomeownerSeeder.php
@@ -12,6 +12,23 @@ class HomeownerSeeder extends Seeder
      */
     public function run(): void
     {
-
+        Homeowner::create([
+            'first_name' => 'Juan',
+            'last_name' => 'Dela Cruz',
+            'middle_name' => 'Santos',
+            'email' => 'homeowner@gmail.com',
+            'phone' => '09171234567',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'avatar' => null,
+            'bio' => 'Test homeowner account',
+            'address' => '123 Sample Street',
+            'city' => 'Auckland',
+            'region' => 'Auckland',
+            'postal_code' => '1010',
+            'latitude' => -36.8485,
+            'longitude' => 174.7633,
+            'status' => 'active',
+        ]);
     }
 }
